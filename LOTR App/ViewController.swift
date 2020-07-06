ef1a02f3b72a373ac0e56a9bdd78b3ba54bfc21c
Abdullatif Atci <latif.800@gmail.com>
2020-07-06 12:53:03 +0300

    Initial Commit

---
+++ LOTR App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LOTR App
+//
+//  Created by Latif Atci on 7/6/20.
+//  Copyright © 2020 Latif Atci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
